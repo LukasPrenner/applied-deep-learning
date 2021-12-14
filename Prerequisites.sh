5cb2d8f971cdea50236666a750fa7437adfd67d3
Lukas <lukas3012@gmail.com>
2021-12-14 15:12:03 +0100

    update Prerequisites.sh

--- Prerequisites.sh
+++ Prerequisites.sh
@@ -3,7 +3,6 @@
 ### assuming pip3 is installed (apt install python3-pip)
 ### assuming python 3.7 is used
 pip3 install mtcnn
-pip3 install Flask
 pip3 install keras # has to be version 2.7.0
 pip3 install tensorflow # has to be version 2.7.0
 pip3 install scikit-image
